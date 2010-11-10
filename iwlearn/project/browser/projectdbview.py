4dadcd181387a5bb226c98876a35a34324468779
Christian Ledermann <christian.ledermann@gmail.com>
2010-11-10 09:09:43 +0000

    add project search to view

--- iwlearn/project/browser/projectdbview.py
+++ iwlearn/project/browser/projectdbview.py
@@ -33,14 +33,40 @@ class ProjectDBView(BrowserView):
     def portal(self):
         return getToolByName(self.context, 'portal_url').getPortalObject()
 
+    def _sel(self, item, selected):
+        if item == selected:
+            return 'selected'
+        else:
+            return None
+
+    def _get_index_values(self, idx):
+        items = list(self.portal_catalog.Indexes[
+                idx].uniqueValues())
+        selected = self.request.get(idx,None)
+        items.sort()
+        item_list =  [{'name': _('All'), 'value':'',
+                'disabled':None,
+                'selected':self._sel(None,selected)}]
+        for item in items:
+            item_list.append({'name': item, 'value':item,
+                    'disabled': None,
+                    'selected':self._sel(item,selected)})
+        return item_list
+
+    def get_projecttype(self):
+        return self._get_index_values('getProject_type')
+
+    def get_agency(self):
+        return self._get_index_values('getAgencies')
+
+    def get_status(self):
+        return self._get_index_values('getProject_status')
+
+    def get_basin(self):
+        return self._get_index_values('getBasin')
 
     def get_region(self):
         """ Returns regions/subregions select """
-        def sel(region):
-            if selected_region == region:
-                return 'selected'
-            else:
-                return None
         def disable(region):
             if region in active_regions:
                 return None
@@ -49,17 +75,17 @@ class ProjectDBView(BrowserView):
 
         active_regions = self.portal_catalog.Indexes[
                 'getSubRegions'].uniqueValues()
-        selected_region = self.request.get('getSubRegions',None)
+        selected = self.request.get('getSubRegions',None)
 
         region_subregions = [{'name': 'All', 'value':'',
                 'disabled':None,
-                'selected':sel(None)}]
+                'selected':self._sel(None,selected)}]
         regions = vocabulary.get_regions()
         for region in regions:
             disabled = disable(region)
             region_subregions.append({'name': region, 'value':region,
                     'disabled': disabled,
-                    'selected':sel(region)})
+                    'selected':self._sel(region,selected)})
             if disabled:
                 continue
             else:
@@ -69,5 +95,38 @@ class ProjectDBView(BrowserView):
                     region_subregions.append({'name': ' - ' + subregion,
                             'value': subregion,
                             'disabled': disabled,
-                            'selected':sel(subregion)})
+                            'selected':self._sel(subregion,selected)})
         return region_subregions
+
+
+    @property
+    def search_term(self):
+        return self.request.form.get('SearchableText', '')
+
+    def search_results(self):
+        form = self.request.form
+        is_search = len(form)!=0
+        if not is_search:
+            return None
+        catalog = self.portal_catalog
+        ptitle = form.get('Title', None)
+        ptype = form.get('getProject_type', None)
+        pagency = form.get('getAgencies', None)
+        pstatus = form.get('getProject_status', None)
+        pbasin = form.get('getBasin', None)
+        pregion = form.get('getSubRegions', None)
+
+        batch_size = form.get('b_size', 20)
+        batch_start = form.get('b_start', 0)
+        is_search = len(form)!=0
+
+        results = catalog(SearchableText=self.search_term,
+                        portal_type='Project',
+                        Title=ptitle,
+                        getProject_type=ptype,
+                        getAgencies=pagency,
+                        getProject_status=pstatus,
+                        getBasin=pbasin,
+                        getSubRegions=pregion)
+
+        return {'results': results, 'size': batch_size, 'start': batch_start}

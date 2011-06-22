2da55c42b197ef5fd9a0406b71376eecb942cd8d
Christian Ledermann <christian.ledermann@gmail.com>
2011-06-22 17:06:02 +0000

    script to geoannotate the countries

---
+++ iwlearn/project/geoannotate_countries.py
@@ -0,0 +1,32 @@
+#
+import csv
+import geojson
+from collective.geo.contentlocations.interfaces import IGeoManager
+from collective.geo.settings.interfaces import IGeoCustomFeatureStyle, IGeoFeatureStyle
+from plone.i18n.locales.countries import _countrylist
+
+def annotate(self):
+    data = csv.DictReader(open('110m_admin_0_countries.csv', 'r'))
+    for brain in self.portal_catalog(portal_type = 'Folder', path='iwlearn/countries/'):
+        obj=brain.getObject()
+    for d in data:
+        #print d['json_4326']
+        #print d['admin']
+        print d['iso_a2']
+        child_id = d['iso_a2'].lower() + '.png'
+        q = geojson.loads(d['json_4326'])
+        print q
+        if d['iso_a2'] != '-99':
+            child = obj[child_id]
+            geo = IGeoManager(child)
+            #geo.setCoordinates(**q)
+            geo.setCoordinates(q['type'], q['coordinates'])
+
+def name_countries(self):
+    for brain in self.portal_catalog(portal_type = 'Image', path='iwlearn/countries/'):
+        cc = brain.getId
+        if cc[:2] in _countrylist:
+            obj = brain.getObject()
+            obj.setTitle(_countrylist[cc[:2]]['name'])
+            print _countrylist[cc[:2]]['name']
+    import ipdb; ipdb.set_trace()

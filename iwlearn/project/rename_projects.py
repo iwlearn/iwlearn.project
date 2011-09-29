0e8cbc66495df63f57bfc7ca18a1b9a756ecefe4
Christian Ledermann <christian.ledermann@gmail.com>
2011-09-29 13:36:45 +0000

    start renaming once only

--- iwlearn/project/rename_projects.py
+++ iwlearn/project/rename_projects.py
@@ -1,35 +1,38 @@
 # rename projects - set objectId = gef_project_id
 import logging
 logger = logging.getLogger('iwlearn.project')
-is_renaming = False
+_is_renaming = False
+
+
+def rename_contained_projects(parent):
+    j = 0
+    for child in parent.objectValues('Project'):
+        j += rename_contained_projects(child)
+        if j > 50:
+            break
+        child_id = child.id
+        obj_id = str(int(child.getGef_project_id().strip()))
+        if obj_id != child_id:
+            try:
+                parent.manage_renameObject(child_id, obj_id)
+                logger.info('renamed Project %s' % obj_id )
+                j += 1
+            except:
+                logger.info('Failed to rename Project from %s to %s' %(child_id, obj_id ))
+        else:
+            logger.info('No need to rename project %s' % obj_id )
+    return j
+
 
 def mv_projects(self):
-    def rename_contained_projects(parent):
-        j = 0
-        for child in parent.objectValues('Project'):
-            j += rename_contained_projects(child)
-            if j > 50:
-                break
-            child_id = child.id
-            obj_id = str(int(child.getGef_project_id().strip()))
-            if obj_id != child_id:
-                try:
-                    parent.manage_renameObject(child_id, obj_id)
-                    logger.info('renamed Project %s' % obj_id )
-                    j += 1
-                except:
-                    logger.info('Failed to rename Project from %s to %s' %(child_id, obj_id ))
-            else:
-                logger.info('No need to rename project %s' % obj_id )
-        return j
-    if is_renaming:
+    if _is_renaming:
         return 'another rename process is running'
     else:
-        is_renaming = True
+        _is_renaming = True
     logger.info( 'search for projects')
     i=0
     parent = self.portal_url.getPortalObject()['iw-projects']
     i = rename_contained_projects(parent)
     logger.info('renaming complete')
-    is_renaming = False
+    _is_renaming = False
     return '%i projects renamed' %i

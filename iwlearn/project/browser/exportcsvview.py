1917e8a825d5b2703f27960193ef67424d687169
Christian Ledermann <christian.ledermann@gmail.com>
2012-11-23 09:43:35 +0000

    fix export

--- iwlearn/project/browser/exportcsvview.py
+++ iwlearn/project/browser/exportcsvview.py
@@ -6,7 +6,7 @@ except:
     from StringIO import StringIO
 
 import csv
-
+from datetime import datetime
 from zope.interface import implements, Interface
 
 from Products.Five import BrowserView
@@ -91,24 +91,36 @@ class ExportCSVView(BrowserView):
         fieldnames = CSV_FIELDS
         writer = csv.DictWriter(output, fieldnames)
         brains = self.portal_catalog(portal_type = 'Project')
+
+        #writer.writeheader()
+        pd = {}
+        for field in fieldnames:
+            pd[field] = field
+        writer.writerow(pd)
+
         for brain in brains:
             pd = {}
             obj = brain.getObject()
             pd['ID']= int(obj.getGef_project_id())
-            pd['Agency']= [self.acronym(e.Title()) for e in obj.getAgencies()]
-            pd['Country']= obj.getCountry()
+            pd['Agency']= '; '.join([self.acronym(e.Title()) for e in obj.getAgencies()])
+            pd['Country']= '; '.join(obj.getCountry())
             pd['Type']= obj.getProject_type()
             pd['Project Name']= obj.Title()
             pd['Region']= obj.getRegion()
             pd['Subregion']= obj.getSubregion()
-            pd['Basin']= obj.getBasin()
+            pd['Basin']= '; '.join(obj.getBasin())
             pd['Project Status']=obj.getProject_status()
-            pd['FOCAL AREA']=obj.getFocal_area()
+            pd['FOCAL AREA']='; '.join(obj.getFocal_area())
             pd['Approval Date']= None
-            pd['Start']= obj.getStart_date()
-            pd['End']= obj.getEnd_date()
+            start = end = None
+            if obj.getStart_date():
+                start = obj.getStart_date().strftime('%Y-%m-%d')
+            if obj.getEnd_date():
+                end = obj.getEnd_date().strftime('%Y-%m-%d')
+            pd['Start']= start
+            pd['End']= end
             pd['FY']= None
-            pd['Staff']= [s.Title() for s in obj.getProject_contacts()]
+            pd['Staff']= '; '.join([s.Title() for s in obj.getProject_contacts()])
             pd['GEFAmount']= obj.getGef_project_allocation()
             pd['Cofin Amt']= None
             pd['Fee Total']= None
@@ -121,12 +133,19 @@ class ExportCSVView(BrowserView):
             pd['Prog App']= None
             pd['Multi FA']= None
             pd['SAP']= None
-            pd['Keywords']= obj.Subject()
+            pd['Keywords']= '; '.join(obj.Subject())
             pd['Url']= obj.getRemoteUrl()
-            pd['Strategic Program']= obj.getStrategic_priority()
-            pd['Operational Program']= obj.getOperational_programme()
-            pd['Executing Agency']= [e.Title() for e in obj.getExecuting_agency()]
+            pd['Strategic Program']= '; '.join(obj.getStrategic_priority())
+            pd['Operational Program']= '; '.join(obj.getOperational_programme())
+            pd['Executing Agency']= '; '.join([e.Title() for e in obj.getExecuting_agency()])
             writer.writerow(pd)
-        import ipdb; ipdb.set_trace()
+        output.seek(0)
+        self.request.RESPONSE.setHeader('Content-Type','text/csv; charset=utf-8')
+        filename = datetime.now().strftime('IWLEARN_Projects_%Y-%m-%d.csv')
+        self.request.response.setHeader(
+            'Content-Disposition',
+            'attachment; filename="%s"' % filename)
+        return output.read()
+
 
 

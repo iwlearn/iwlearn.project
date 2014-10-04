5da716abe296bb8e5b8a229376a334747e5875cd
Jean Jordaan <jean.jordaan@gmail.com>
2014-10-04 11:58:16 +0000

    Factor out geo- fields, their indexers, and expressions for computing them

--- iwlearn/project/extender.py
+++ iwlearn/project/extender.py
@@ -1,22 +1,19 @@
-from zope.component import adapts
+""" Extend some types with geo- fields
+"""
+
+import logging
+
+#from zope.component import adapts
 from zope.interface import implements
+from zope.interface import Interface 
+
+from Products.CMFCore.interfaces import ISiteRoot
 
 from Products.ATContentTypes.interfaces import IATDocument
 from Products.ATContentTypes.interfaces import IATFile
 from Products.ATContentTypes.interfaces import IATImage
 
-from Products.Archetypes.public import ComputedField
-from Products.Archetypes.public import ComputedField
-from Products.Archetypes.public import ImageField
-from Products.Archetypes.public import LinesField
-from Products.Archetypes.public import ReferenceField
-from Products.Archetypes.public import StringField
-
-from Products.Archetypes.public import ComputedWidget
-from Products.Archetypes.public import ImageWidget
-from Products.Archetypes.public import InAndOutWidget
-from Products.Archetypes.public import SelectionWidget
-from Products.Archetypes.public import StringWidget
+from Products.Archetypes import atapi
 
 from archetypes.referencebrowserwidget.widget import ReferenceBrowserWidget
 from archetypes.schemaextender.field import ExtensionField
@@ -24,25 +21,91 @@ from archetypes.schemaextender.interfaces import ISchemaExtender
 from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
 from iwlearn.project import vocabulary
 
-# class _ExtensionImageField(ExtensionField, ImageField): pass
-# class _ExtensionStringField(ExtensionField, StringField): pass
-# class _ExtensionCountryField(ExtensionField, LinesField): pass
-# class _ExtensionRegionField(ExtensionField, LinesField):pass
-# class _ExtensionBasinField(ExtensionField, ReferenceField):pass
-class _ExtensionDocumentTypeField(ExtensionField, LinesField):pass
+from iwlearn.project import projectMessageFactory as _
+from iwlearn.project.interfaces import IProject
+
+logger = logging.getLogger('iwlearn.project')
+
+class _ExtensionComputedField(ExtensionField, atapi.ComputedField): pass
+class _ExtensionImageField(ExtensionField, atapi.ImageField): pass
+class _ExtensionLinesField(ExtensionField, atapi.LinesField): pass
+class _ExtensionReferenceField(ExtensionField, atapi.ReferenceField): pass
+class _ExtensionStringField(ExtensionField, atapi.StringField): pass
+
+class GeoFieldsExtender(object):
+    """ Group Region/Basin/Country fields
+    """
+    implements(ISchemaExtender)
+
+    fields = [
+
+        _ExtensionComputedField('region',
+            schemata='geodata',
+            required=True,
+            searchable=True,
+            expression='context.restrictedTraverse("@@geo_view")._computeRegions()',
+            widget=atapi.ComputedWidget(
+                label=_(u"Geographic Region"),
+                description=_(u"Geographic Region in which the project operates"),
+            ),
+        ),
+
+        _ExtensionComputedField('subregion',
+            schemata='geodata',
+            required=False,
+            searchable=True,
+            expression='context.restrictedTraverse("@@geo_view")._computeSubregions()',
+            widget=atapi.ComputedWidget(
+                label=_(u"Geographic Sub Region"),
+                description=_(u"Geographic Sub Region in which the project operates"),
+            ),
+        ),
+
+        _ExtensionLinesField('country',
+            schemata='geodata',
+            required=False,
+            searchable=True,
+            vocabulary=vocabulary.get_countries(),
+            widget=atapi.InAndOutWidget(
+                label=_(u"Countries"),
+                description=_(u"Countries"),
+            ),
+        ),
+
+        _ExtensionReferenceField('basins',
+            schemata='geodata',
+            required=False,
+            widget=ReferenceBrowserWidget(
+                label=_(u"Basins"),
+                description=_(u"Basins"),
+                allow_sorting=True,
+            ),
+            relationship='basins_projects',
+            allowed_types=('Basin',), # specify portal type names here ('Example Type',)
+            multiValued=True,
+        ),
+
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+
 
 class ProjectFieldsExtender(object):
     """ Add 'Project Type' field
     """
-    #adapts(IATFile,IATImage)
     implements(ISchemaExtender)
 
     fields = [
-        _ExtensionDocumentTypeField(
+        _ExtensionLinesField(
             "document_type",
             searchable=True,
             vocabulary = vocabulary.DOCUMENT_TYPE,
-            widget = SelectionWidget(
+            widget = atapi.SelectionWidget(
                 label=u"Document Type",
                 description=u"Classification of the document type",
             ),
@@ -58,10 +121,109 @@ class ProjectFieldsExtender(object):
 
 from plone.indexer import indexer
 
-@indexer(IATDocument)
-def document_type_doc_indexer(context):
-    return context.getField('document_type').get(context)
 
 @indexer(IATFile)
-def document_type_file_indexer(context):
+@indexer(IATImage)
+@indexer(IATDocument)
+def document_type_indexer(context):
     return context.getField('document_type').get(context)
+
+
+@indexer(IATFile)
+@indexer(IATImage)
+@indexer(IProject)
+@indexer(IATDocument)
+def country_indexer(context):
+    return context.getField('country').get(context)
+
+
+@indexer(IATFile)
+@indexer(IATImage)
+@indexer(IProject)
+@indexer(IATDocument)
+def country_code_indexer(context):
+    country = context.getField('country').get(context)
+    ccs = []
+    for k,v in vocabulary.my_countrylist.iteritems():
+        if v['name'] in country:
+            ccs.append(k)
+    return ccs
+
+
+@indexer(IATFile)
+@indexer(IATImage)
+@indexer(IProject)
+@indexer(IATDocument)
+def basin_indexer(context):
+    basins = context.getField('basins').get(context)
+    titles = []
+    for basin in basins:
+        if basin is not None:
+             titles.append(basin.Title())
+    return titles
+
+
+@indexer(IATFile)
+def subregion_indexer(context):
+    logger.info('subregion_indexer')
+    countries = context.getField('country').get(context)
+    if context.getProject_scale():
+        scale = [context.getProject_scale(), ]
+    else:
+        scale = []
+    if countries:
+        sr = vocabulary.get_subregions(countries=countries)
+        r = vocabulary.get_regions(countries=countries)
+        return scale + r + sr
+    else:
+        if context.getGlobalproject():
+            return [u'Global',]
+        else:
+            logger.info('no regions found for %s' % '/'.join(
+                context.getPhysicalPath()))
+            return scale + ['???',]
+
+
+class IGeoTags(Interface):
+    """ For content that knows where it is
+    """
+
+    def _computeRegions():
+        """Compute regions"""
+
+    def _computeSubregions():
+        """Compute sub regions"""
+
+
+def _find_first(context, fieldname):
+    context = context.aq_inner
+    while not ISiteRoot.providedBy(context):
+        field_getter = getattr(context, 'getField', None)
+        if field_getter:
+            field = field_getter(fieldname)
+            if field:
+                value = field.get(context)
+                if value:
+                    return value
+        context = context.aq_parent
+
+class GeoTags(object):
+    implements(IGeoTags)
+
+    def _computeRegions(self):
+        context = self.context
+        countries = _find_first(context, 'country')
+
+        if context.getGlobalproject():
+            return ', '.join(
+                    vocabulary.get_regions(
+                        countries=countries,
+                        regions=[u'Global']))
+
+        return ', '.join(vocabulary.get_regions(countries=countries))
+
+    def _computeSubregions(self):
+        context = self.context
+        return ', '.join(
+                vocabulary.get_subregions(
+                        countries=_find_first(context, 'country')))
